ac68aedafca61fdafaf85a658277dec5158bf2f6
Gabriel Vitor Galvão Carlota <gabriel.carlota@alunos.unis.edu.br>
2024-09-28 11:54:03 -0300

    ./

---
+++ SupportGestor/Models/Atendimento.cs
@@ -0,0 +1,14 @@
+﻿
+namespace SupportGestor.Models
+
+{
+    public class Atendimento
+    {
+        public int Id { get; set; }
+        public string NomeAtendente{ get; set; }
+        public string Cliente { get; set; }
+        public string Problema { get; set; }
+        public int NivelDificuldade { get; set; }
+
+    }
+}
---
+++ SupportGestor/ViewModels/AtendimentoVm.cs
@@ -0,0 +1,25 @@
+﻿using SupportGestor.Models;
+using System.Threading.Tasks;
+
+namespace SupportGestor.ViewModels
+{
+    public class AtendimentoViewModel
+    {
+        public string Nome { get; set; }
+        public string Cliente { get; set; }
+        public string Problema { get; set; }
+        public string NotasAtendimento { get; set; }
+        public int NivelDificuldade { get; set; }
+
+        public async Task Salvar()
+        {
+            var atendimento = new Atendimento
+            {
+                NomeAtendente = Nome,
+                Cliente = Cliente,
+                Problema = Problema,
+                NivelDificuldade = NivelDificuldade
+            };
+        }
+    }
+}
